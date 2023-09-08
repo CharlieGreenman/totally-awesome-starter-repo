21fb83172c4543bb91c3b7b6c156ca09a8ad710a
Charlie Greenman <charlie@razroo.com>
2023-09-07 22:26:27 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/totally-awesome-starter-repo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'totally-awesome-starter-repo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'totally-awesome-starter-repo';
+}
